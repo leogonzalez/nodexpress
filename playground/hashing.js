3972d52a0a2caaebb43e7e990f0a66709a769f6a
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2018-04-04 01:01:08 -0300

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,11 +1,26 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+const password = '123abs';
+bcrypt.genSalt(10, (err, salt) => {
+  console.log(`salt`,salt);
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(`hash`,hash);
+  })
+})
 
-var token = jwt.sign(data, '123ABC');
+var hashPass = '$2a$10$PGNq/jGxacfk/KYjoESC0eEwVpfPEUsEkr7oqoggMNBpWS3zU9v1a';
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded ', decoded);
+bcrypt.compare(password, hashPass, (err, res) => {
+  console.log(`res`,res);
+});
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123ABC');
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded ', decoded);
